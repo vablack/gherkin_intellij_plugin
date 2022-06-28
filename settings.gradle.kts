c6a7684b211a4d9a91fc36cb8e145e43cb6a90f7
GitHub Action <action@github.com>
2022-06-28 04:22:54 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "gherkin_intellij_plugin"
---
+++ src/main/kotlin/com/github/vablack/gherkinintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vablack.gherkinintellijplugin.services
+
+import com.github.vablack.gherkinintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
